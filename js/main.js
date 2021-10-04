a54c587b1fa6d230bb9214e4117f36d87eb071cd
chikamasuda <c.m.062820@gmail.com>
2021-10-04 20:37:54 +0900

    ローカルストレージに保存するところまで完了

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-(function() {
+(function () {
   'use strict';
 
-  var vm = new Vue ({
+  var vm = new Vue({
     el: '#app',
     data: {
       newItem: '',
-      todos: [{
-        title: 'task 1',
-        isDone: false
-      }, {
-        title: 'task 2',
-        isDone: false
-      }, {
-        title: 'task 3',
-        isDone: true
-      }]
+      todos: []
+    },
+    watch: {
+      todos: {
+        handler: function () {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deep: true
+      }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
-      addItem: function() {
+      addItem: function () {
         var item = {
           title: this.newItem,
           isDone: false
@@ -25,18 +27,23 @@
         this.todos.push(item);
         this.newItem = "";
       },
-      deleteItem: function(index) {
+      deleteItem: function (index) {
         if (confirm('are you sure ?')) {
           this.todos.splice(index, 1);
         }
+      },
+      purge: function () {
+        if (!confirm('delete finished?')) {
+          return;
+        }
+        this.todos = this.remaining;
       }
     },
     computed: {
-      remaining: function() {
-        var items = this.todos.filter(function(todo) {
+      remaining: function () {
+        return this.todos.filter(function (todo) {
           return !todo.isDone;
         });
-        return items.length;
       }
     }
   });
